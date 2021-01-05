2deb6b4b9b1799c390ea14806a7d7c52ad1ce471
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-05 17:47:39 +0000

    Keep forgetting to commit, bad Jamie

--- cludo.js
+++ cludo.js
@@ -105,22 +105,95 @@
 
 // Episode VI - Return of the Jedi
 
-let murderer = 'Colonel Mustard';
+// let murderer = 'Colonel Mustard';
+// // murderer would change to 'Mr. Green' then to Mr. White this would be a global variable 
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
 
-const changeMurderer = function() {
-  murderer = 'Mr. Green';
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
 
-  const plotTwist = function() {
-    murderer = 'Mrs. White';
+//   plotTwist();
+// }
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+// // as plot twist is inside of change murderer should be mr White 
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Episode VII - The Force Awakens
+// let murderer = 'Professor Plum';
+// // a very confusing function it first assigns murderer to mr Green 
+// // then to miss Scarlet 
+// // then with the plot twist to colonel mustard 
+// // had to read it twice to make sure of the order of functions being called inside 
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+// // even though it is declared first i think it will be run second making this the murderer of the function 
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+//   plotTwist();
+// }
+// const declareMurderer = function() {
+//     return `The murderer is ${murderer}.`;
+// }
+
+// // should return colonel mustard i believe 
+// // didnt it returned mr green, he is declared as a global variable 
+// // ahh becuase after the plot twist is finished 
+// // inside the scope of the original change function murderer is till mr green..?
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Episode VIII - The Last Jedi... not really tho..
+
+const scenario = {
+    murderer: 'Mrs. Peacock',
+    room: 'Conservatory',
+    weapon: 'Lead Pipe'
+  };
+  // when called below, the function first changes conservatory to dining room 
+  // then it runs plot twist which changes the murderer to colonel mustard
+  // which inturn runs unexpected outcome which changes the weapon to candle stick 
+  //
+  const changeScenario = function() {
+    scenario.murderer = 'Mrs. Peacock';
+    scenario.room = 'Dining Room';
+  
+    const plotTwist = function(room) {
+      if (scenario.room === room) {
+        scenario.murderer = 'Colonel Mustard';
+      }
+  
+      const unexpectedOutcome = function(murderer) {
+        if (scenario.murderer === murderer) {
+          scenario.weapon = 'Candle Stick';
+        }
+      }
+  
+      unexpectedOutcome('Colonel Mustard');
+    }
+  
+    plotTwist('Dining Room');
   }
-
-  plotTwist();
-}
-
-const declareMurderer = function () {
-  return `The murderer is ${murderer}.`;
-}
-
-changeMurderer();
-const verdict = declareMurderer();
-console.log(verdict);
+  
+  const declareWeapon = function() {
+    return `The weapon is ${scenario.weapon}.`
+  }
+  
+  changeScenario();
+  const verdict = declareWeapon();
+  // i think the weapon should be candle stick
+  console.log(verdict);
